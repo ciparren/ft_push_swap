a736f2acb83664ed3ba8c0955c2df8b08f7706f5
Cintia <ciparren@student.42madrid.com>
2026-02-18 17:55:50 +0100

    archivos obligatorios: makefile, push_swap y la librería

---
+++ push_swap.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cintia <cintia@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 17:47:48 by cintia            #+#    #+#             */
+/*   Updated: 2026/02/18 17:55:23 by cintia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// los argumentos que recibo son:
+    // una pila a formateada como una lista de enteros
+    // un parámetro opcional de selector de algoritmo(simple, medium, complex, adaptative)
